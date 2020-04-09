ac72fc0d0b164d80bd955819abfeb7598c5db80d
JakeEgarhos <jakee1354@gmail.com>
2020-04-09 01:35:17 -0700

    Add working edit user - user can now change their password

--- editUser.php
+++ editUser.php
@@ -1,8 +1,16 @@
 <?php
 
 session_start();
-editUser();
-header('Location: index.php');
+$editCheck = editUser();
+
+if ($editCheck != null)
+echo '<p>Password succedfully changed! Go back to main page:</p>
+      <a class="btn btn-secondary btn-sm" href="index.php" role="button">Return</a>'; //edit success
+else
+echo '<p>That user is not in the database! Go back to previous page:</p>
+      <a class="btn btn-secondary btn-sm" href="userinfo.php" role="button">Return</a>'; //failed edit
+
+
 
 function editUser(){
 
@@ -19,9 +27,40 @@ function editUser(){
 
     include 'db_connection.php';
 
-    
+    try {
+      $sql = "SELECT * FROM profile WHERE userName = ?";
+        $retStr = null;
 
+        //get connection
+        $pdo = openConnection();
+          $stmt = $pdo->prepare($sql);
+            $stmt->execute([$user]);
+            while($row = $stmt->fetch()) {
+              $retStr = $row['userID'];}
+              //if the user is in the system, change the password
+              if ($retStr != null){
+                $sql = "UPDATE profile
+                        SET password = ?
+                        WHERE userName = ?";
+
+
+                  $pdo = openConnection();
+                    $stmt = $pdo->prepare($sql);
+                      $stmt->execute([$pw, $user]);
+              }
+
+
+
+
+              }
+                     catch(\PDOException $e) {
+                      echo '<p>There was an error connecting to the database! Go back to previous page:</p>
+                            <a class="btn btn-secondary btn-sm" href="userinfo.php" role="button">Return</a>';
+                     }
+                       return $retStr;
 
 }
+
+
 
  ?>
